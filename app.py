adc0f2844299237768470416788fd33094a52a10
Cristianbenz <contactocbenitez@gmail.com>
2022-12-04 16:02:30 -0300

    fix(app.py): fix db query into update controller

--- app.py
+++ app.py
@@ -12,9 +12,12 @@ host = environ.get('DB_HOST')
 port = environ.get('DB_PORT')
 database = environ.get('DB_NAME')
 
+
 def get_connection():
-    conn = connect(user=user, password=password, host=host, port=port, database=database)
+    conn = connect(user=user, password=password,
+                   host=host, port=port, database=database)
     return conn
+
 
 @app.get('/api/restaurants')
 def get_restaurants():
@@ -28,8 +31,9 @@ def get_restaurants():
 
     if len(result) == 0:
         return jsonify({"message": "Not exist any restaurant yet"}), 404
-    
+
     return jsonify(result)
+
 
 @app.post('/api/restaurant')
 def create_restaurant():
@@ -58,9 +62,10 @@ def create_restaurant():
     conn.close()
 
     if new_restaurant is None:
-        return jsonify({"message":"Restaurant not found"}), 404
+        return jsonify({"message": "Restaurant not found"}), 404
 
     return jsonify(new_restaurant)
+
 
 @app.put('/api/restaurant/<restaurant_id>')
 def modify_restaurant(restaurant_id):
@@ -79,8 +84,8 @@ def modify_restaurant(restaurant_id):
     lng = data["lng"]
 
     cur.execute(
-        "UPDATE restaurants SET (rating, name, site, email, phone, street, city, state, lat, lng) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) WHERE id = %s RETURNING *",
-    (rating, name, site, email, phone, street, city, state, lat, lng, restaurant_id))
+        "UPDATE restaurants SET rating= %s, name= %s, site= %s, email= %s, phone= %s, street= %s, city= %s, state= %s, lat= %s, lng= %s WHERE id=%s RETURNING *",
+        (rating, name, site, email, phone, street, city, state, lat, lng, restaurant_id))
 
     conn.commit()
     modified_restaurant = cur.fetchone()
@@ -88,6 +93,7 @@ def modify_restaurant(restaurant_id):
     conn.close()
 
     return jsonify(modified_restaurant)
+
 
 if __name__ == "__main__":
     app.run(port=environ.get('PORT'), debug=True)
