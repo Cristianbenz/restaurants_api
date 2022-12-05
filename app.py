3077f09ec3a2e76d4c5f0a0335732a0263264025
Cristian Benitez <91490781+Cristianbenz@users.noreply.github.com>
2022-12-05 11:50:35 -0300

    merge development into deploy (#3)
    * fix(db.py): move preload ejecution into db file
    
    * add readme as index route

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from dotenv import load_dotenv
+import markdown
 load_dotenv()
 
 app = Flask(__name__)
+
+@app.get('/')
+def index():
+    with open("README.md") as readme_file:
+        readme = markdown.markdown(
+            readme_file.read(), extensions=["fenced_code"]
+        )
+
+    return readme
 
 from controllers import *
