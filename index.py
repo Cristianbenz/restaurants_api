6bbc9658e478b32336029ba76d1b7d49acb38397
Cristian Benitez <91490781+Cristianbenz@users.noreply.github.com>
2022-12-05 10:19:47 -0300

    set app debug mode to false

--- index.py
+++ index.py
@@ -5,4 +5,4 @@ from db import get_connection
 
 if __name__ == '__main__':
     parse_info(get_connection)
-    app.run(port=environ.get('PORT'), debug=True)
+    app.run(port=environ.get('PORT'), debug=False)
